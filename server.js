777eeefca63c25e7a156318a6da43887688476cb
Jaaarl <jarl13.work@gmail.com>
2025-05-04 06:36:58 +0800

    add: cors

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors');
+
 dotenv.config();
 
 const authRoutes = require('./routes/auth');
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
